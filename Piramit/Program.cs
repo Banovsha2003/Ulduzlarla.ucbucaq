bc77eeb0e3a95a757db3c8cb38f8fbe324fe19bd
Banovsha2003 <78psrjd@code.edu.az>
2023-11-21 18:52:46 +0400

    Add project files.

---
+++ Piramit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Piramit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("üçbucaq:");
+            for (int i = 1; i <15; i++)
+            {
+                for (int j = i; j < 15; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+        
+        }
+    
+
